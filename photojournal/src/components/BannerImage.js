eeb063026aef5fd0f312f1cd9c90db9387080da6
Alexandru Visarion <viorel.visarion@gmail.com>
2022-11-19 14:02:29 +0000

    fix

---
+++ photojournal/src/components/BannerImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './master.css'
+
+function BannerImage() {
+  return (
+    <div>
+        <img className="banner" src = "../images/banner.jpg" alt="Banner image" />
+    </div>
+  );
+}
+
+export default BannerImage;
